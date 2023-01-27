8868f361c53ee19dca90f326eae0077e4211027f
tlmn <mail@tlmn.io>
2023-01-27 22:42:56 +0100

    add stitching

--- app/app.py
+++ app/app.py
@@ -1,10 +1,27 @@
-from flask import Flask
+from flask import Flask, send_file, request
+import cv2
+import numpy as np
 
 app = Flask(__name__)
 
 @app.route("/stitchPanorama", methods=["POST"])
 def stitchPanorama():
-    return "<h1>trying to stitch</h1>"
+    image_files = request.files.getlist("images")
+    imgs = []
+
+    for image_file in image_files:
+        img = cv2.imdecode(np.fromstring(image_file.read(), np.uint8), cv2.IMREAD_UNCHANGED)
+        imgs.append(img)
+    
+    stitchy=cv2.Stitcher.create()
+
+    (dummy,output)=stitchy.stitch(imgs)
+    
+    if dummy != cv2.STITCHER_OK:
+        return "not successful"
+    else: 
+        cv2.imwrite('stitched_panorama.jpg', output)
+        return send_file('stitched_panorama.jpg', mimetype='image/jpeg')
 
 if __name__ == "__main__":
     from waitress import serve
