e0a5d1874e9f5f05846f5208e174c5ebb2b7435e
tlmn <mail@tlmn.io>
2023-01-28 12:34:42 +0100

    make stitcher work

--- app/app.py
+++ app/app.py
@@ -1,17 +1,26 @@
 from flask import Flask, send_file, request
 import cv2
 import numpy as np
+from flask_cors import CORS
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/stitchPanorama", methods=["POST"])
 def stitchPanorama():
     image_files = request.files.getlist("images")
     imgs = []
+    logging.debug("image_files", image_files)
 
     for image_file in image_files:
-        img = cv2.imdecode(np.fromstring(image_file.read(), np.uint8), cv2.IMREAD_UNCHANGED)
+        # img = cv2.imdecode(np.fromstring(image_file.read(), np.uint8), cv2.IMREAD_UNCHANGED)
+        img = cv2.imdecode(np.fromstring(image_file.read(), np.uint8), 1)
         imgs.append(img)
+        logging.debug("image_file appended")
+
     
     stitchy=cv2.Stitcher.create()
 
@@ -20,8 +29,8 @@ def stitchPanorama():
     if dummy != cv2.STITCHER_OK:
         return "not successful"
     else: 
-        cv2.imwrite('stitched_panorama.jpg', output)
-        return send_file('stitched_panorama.jpg', mimetype='image/jpeg')
+        cv2.imwrite('stitched_panorama.png', output)
+        return send_file('stitched_panorama.png', mimetype='image/png')
 
 if __name__ == "__main__":
     from waitress import serve
